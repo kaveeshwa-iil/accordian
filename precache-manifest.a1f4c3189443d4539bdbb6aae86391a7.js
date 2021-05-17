24a626d27c175c35d6cd9ee63892d0866efd4d46
kaveeshwa-iil <kaveeshwa.b@informaticsint.com>
2021-05-17 22:56:13 +0530

    Updates

---
+++ precache-manifest.a1f4c3189443d4539bdbb6aae86391a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8300259dc9e36597f7fb6c27a24e49f3",
+    "url": "/accordian/index.html"
+  },
+  {
+    "revision": "8f76e4eed739e662a55f",
+    "url": "/accordian/static/css/main.225ac162.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f76e4eed739e662a55f",
+    "url": "/accordian/static/js/main.718fd488.chunk.js"
+  },
+  {
+    "revision": "b9012adf2d0eb94649e5",
+    "url": "/accordian/static/js/runtime-main.af1fad76.js"
+  }
+]);
